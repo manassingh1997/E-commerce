c92939c7f1380ece4f97d19fc575a7b793d1e918
Manas Singh <manassinghsolanki@hotmail.com>
2025-01-13 17:11:58 +0530

    Resolved the bug in urls.py

--- e_comm/urls.py
+++ e_comm/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
-urlspattern = [
-    path('', index, name='index.html'),
+urlpatterns = [
+    path('', index, name='index'),
 ]
